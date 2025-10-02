94453d22f3d207417a937b82e1a38df07778c105
Lucas Brenner <lucasbref82@gmail.com>
2025-10-02 10:30:00 -0300

    Resolvendo conflitos

--- src/main/java/orientacao_objetos/model/Triangulo.java
+++ src/main/java/orientacao_objetos/model/Triangulo.java
@@ -2,12 +2,9 @@ package orientacao_objetos.model;
 
 public class Triangulo {
     public double a, b, c;
-<<<<<<< HEAD
 
     public double CalculaArea() {
         double p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
-=======
->>>>>>> 056880e2efcede7f863e4649b30088bbac68654b
 }
