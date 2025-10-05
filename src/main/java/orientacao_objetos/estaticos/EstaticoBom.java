e8d6c13f29fe45ac7f65d926d75e87a65270f8b4
Lucas Brenner <lucasbref82@gmail.com>
2025-10-04 22:21:37 -0300

    Implementando método que vai usar a calculadora

--- src/main/java/orientacao_objetos/estaticos/EstaticoBom.java
+++ src/main/java/orientacao_objetos/estaticos/EstaticoBom.java
@@ -1,19 +1,21 @@
 package orientacao_objetos.estaticos;
 
+import orientacao_objetos.estaticos.utils.CalculadoraUtils;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class EstaticoMediano {
+public class EstaticoBom {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         try (Scanner scanner = new Scanner(System.in)){
             System.out.print("Digite o raio: ");
             double raio = scanner.nextDouble();
-            double circunferencia = calculaCircunferencia(raio);
-            double volume = calculaVolume(raio);
+            double circunferencia = CalculadoraUtils.calculaCircunferencia(raio);
+            double volume = CalculadoraUtils.calculaVolume(raio);
             System.out.printf("Circunferencia: %.2f%n", circunferencia);
             System.out.printf("Volume: %.2f%n", volume);
-            System.out.printf("PI: %.2f%n", PI);
+            System.out.printf("PI: %.2f%n", CalculadoraUtils.PI);
         }
 
     }
