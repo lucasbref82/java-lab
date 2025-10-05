7ad43d3990cd614e2e7dadd8c6572d8b0ae93dfc
Lucas Brenner <lucasbref82@gmail.com>
2025-10-05 09:11:51 -0300

    Criação de um utilitario para fazer conversões de dolar para reais + IOF

--- src/main/java/orientacao_objetos/estaticos/utils/ConversorUtils.java
+++ src/main/java/orientacao_objetos/estaticos/utils/ConversorUtils.java
@@ -1,4 +1,19 @@
 package orientacao_objetos.estaticos.utils;
 
-public class ConversorUtils {
+public final class ConversorUtils {
+
+    private ConversorUtils() throws IllegalAccessException {
+        throw new IllegalAccessException("Classe utilitária não pode ser instanciada.");
+    }
+    private static final int PORCENTAGEM_ACRESCIMO = 6;
+    private static final int DIVIDENDO_PORCENTAGEM = 100;
+
+    public static double conversorDolarEmReais(double valorDolar, double quantidadeDolar) {
+        double valorTotal = valorDolar * quantidadeDolar;
+        return calculaAcrecimoPorcentagem(valorTotal);
+    }
+
+    private static double calculaAcrecimoPorcentagem(double valorAcrescentado) {
+        return (double) PORCENTAGEM_ACRESCIMO / DIVIDENDO_PORCENTAGEM * valorAcrescentado + valorAcrescentado;
+    }
 }
