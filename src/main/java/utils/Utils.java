1833a7c52aebd5609d64bf6993596c47a7b9a3d2
Lucas Brenner <lucasbref82@gmail.com>
2025-10-11 00:46:27 -0300

    Criando classe utilitaria

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -1,4 +1,14 @@
 package utils;
 
+import org.slf4j.helpers.MessageFormatter;
+
 public class Utils {
+
+    private Utils() throws IllegalAccessException {
+        throw new IllegalAccessException("Classe utilitária não pode ser instanciada.");
+    }
+
+    public static String format(String texto, Object... argumentos) {
+        return MessageFormatter.basicArrayFormat(texto, argumentos);
+    }
 }
