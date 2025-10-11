c2d9a26184fe8859302b0277151e97ad3f7daa24
Lucas Brenner <lucasbref82@gmail.com>
2025-10-11 10:53:36 -0300

    Criando métodos utilitários para serem utilizados ao longo do repo

--- src/main/java/utils/Utils.java
+++ src/main/java/utils/Utils.java
@@ -11,4 +11,20 @@ public class Utils {
     public static String format(String texto, Object... argumentos) {
         return MessageFormatter.basicArrayFormat(texto, argumentos);
     }
+
+    public static String toString(Object objeto) {
+        try {
+            return objeto.toString();
+        } catch (ClassCastException e) {
+            return null;
+        }
+    }
+
+    public static String toStringOrDefault(Object objeto, String valorPadrao) {
+        try {
+            return objeto.toString();
+        } catch (ClassCastException e) {
+            return valorPadrao;
+        }
+    }
 }
