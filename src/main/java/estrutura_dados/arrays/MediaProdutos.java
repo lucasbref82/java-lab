f0cd2e545a6f7815944c377cf4450a1c072ff321
Lucas Brenner <lucasbref82@gmail.com>
2025-10-11 19:05:10 -0300

    Log mais robusto

--- src/main/java/estrutura_dados/arrays/MediaProdutos.java
+++ src/main/java/estrutura_dados/arrays/MediaProdutos.java
@@ -8,6 +8,7 @@ import utils.Utils;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MediaProdutos {
@@ -15,28 +16,40 @@ public class MediaProdutos {
     private static final Logger log = LoggerFactory.getLogger(MediaProdutos.class);
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)){
+        try (Scanner scanner = new Scanner(System.in)) {
+            scanner.useLocale(Locale.US);
+
             System.out.println("Digite a quantidade de produtos a serem armazenados.");
             int n = scanner.nextInt();
+            scanner.nextLine();
+
+            if (n <= 0) {
+                System.out.println("Quantidade inválida (deve ser maior que zero).");
+                return;
+            }
+
             Produto[] produtos = new Produto[n];
-            for(int i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 System.out.println("Digite os dados do produto!");
                 System.out.print("Nome: ");
-                scanner.nextInt();
                 String nome = scanner.nextLine();
+
                 System.out.print("Preço: ");
                 BigDecimal preco = scanner.nextBigDecimal();
+                scanner.nextLine();
+
                 produtos[i] = new Produto(nome, preco);
             }
+
             BigDecimal precoProdutos = Arrays.stream(produtos)
                     .map(Produto::getPreco)
                     .reduce(BigDecimal.ZERO, BigDecimal::add);
 
-            BigDecimal media = precoProdutos.divide(BigDecimal.valueOf(n), RoundingMode.HALF_UP);
+            BigDecimal media = precoProdutos.divide(BigDecimal.valueOf(n), 2, RoundingMode.HALF_UP);
 
             System.out.println(Utils.format("A média dos produtos é : {}", media));
         } catch (Exception e) {
-            log.error("Aconteceu um erro ao fazer a operação.", e);
+            log.error(Utils.format("Aconteceu um erro ao fazer a operação : {}", e.getMessage()), e);
         }
     }
 }
