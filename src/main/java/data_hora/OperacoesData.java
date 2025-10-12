3762312045b84864fb91fcccb31100fd3cc016dd
Lucas Brenner <lucasbref82@gmail.com>
2025-10-12 11:38:16 -0300

    Formatando data e hora com LocalDateTime

--- src/main/java/data_hora/OperacoesData.java
+++ src/main/java/data_hora/OperacoesData.java
@@ -3,10 +3,13 @@ package data_hora;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class OperacoesData {
-
     public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
         LocalDate d1 = LocalDate.now(); // Data Local
         LocalDateTime d2 = LocalDateTime.now(); // Data e Hora local
         Instant d3 = Instant.now(); // Sempre usa o padrão GMT
@@ -15,6 +18,11 @@ public class OperacoesData {
         Instant d6 = Instant.parse("2022-07-20T01:30:26Z"); // Gera a data hora em string em um horário de Londres.
         Instant d7 = Instant.parse("2022-07-20T01:30:26-03:00"); // Recupera uma String no no formato de londes e dimunui a quantidade de horas necessárias para entrar no padrão America/São Paulo
 
+        LocalDate d8 = LocalDate.parse("20/07/2022", dateTimeFormatter1);
+        LocalDateTime d9 = LocalDateTime.parse("20/07/2022 01:30", dateTimeFormatter2);
+        LocalDate d10 = LocalDate.of(2022, 7, 12);
+        LocalDateTime d11 = LocalDateTime.of(2025, 10, 12, 11, 37);
+
         System.out.println(d1);
         System.out.println(d2);
         System.out.println(d3);
@@ -22,5 +30,9 @@ public class OperacoesData {
         System.out.println(d5);
         System.out.println(d6);
         System.out.println(d7);
+        System.out.println(d8);
+        System.out.println(d9);
+        System.out.println(d10);
+        System.out.println(d11);
     }
 }
