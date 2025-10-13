8ce41d0f591f7173e1f5a1fda509708aac672bbc
Lucas Brenner <lucasbref82@gmail.com>
2025-10-13 11:40:14 -0300

    Correção de atributos e ajustes no programa

--- src/main/java/composicao/entidades/HoraContrato.java
+++ src/main/java/composicao/entidades/HoraContrato.java
@@ -1,17 +1,18 @@
 package composicao.entidades;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class HoraContrato {
-    private LocalDateTime data;
+    private LocalDate data;
     private BigDecimal valorPorHora;
     private Integer horas;
 
     public HoraContrato() {
     }
 
-    public HoraContrato(LocalDateTime data, BigDecimal valorPorHora, Integer horas) {
+    public HoraContrato(LocalDate data, BigDecimal valorPorHora, Integer horas) {
         this.data = data;
         this.valorPorHora = valorPorHora;
         this.horas = horas;
--- src/main/java/composicao/principal/Principal.java
+++ src/main/java/composicao/principal/Principal.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import utils.Utils;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -20,9 +21,13 @@ public class Principal {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)) {
             scanner.useLocale(Locale.US);
-            System.out.print("Qual o nome do departamento?");
+
+            // Criação do departamento
+            System.out.print("Qual o nome do departamento? ");
             String nomeDepartamento = scanner.nextLine();
             Departamento departamento = new Departamento(nomeDepartamento);
+
+            // Trabalhador e hora contrato
             System.out.println("Entre com os dados do trabalhador: ");
             System.out.print("Nome: ");
             String nomeTrabalhador = scanner.nextLine();
@@ -30,22 +35,30 @@ public class Principal {
             Senioridade senioridade = Senioridade.valueOf(scanner.nextLine());
             System.out.print("Salário base: ");
             BigDecimal salarioBase = scanner.nextBigDecimal();
-            System.out.print("Quantos contratos tem esse trabalhador ?");
+            System.out.print("Quantos contratos tem esse trabalhador? ");
             Integer quantidadeContratos = scanner.nextInt();
+
+            // Cria a lista de contratos e percorre por ela.
             List<HoraContrato> horaContratos = new ArrayList<>();
             for (int i = 1; i <= quantidadeContratos; i++) {
-                System.out.println("Entre com os dados do contradto #" + i);
+                scanner.nextLine();
+                System.out.println("Entre com os dados do contrato #" + i);
                 System.out.print("Data (DD/MM/YYYY): ");
-                LocalDateTime dataContrato = LocalDateTime.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                String dtContrato = scanner.nextLine();
+                LocalDate dataContrato = LocalDate.parse(dtContrato, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
                 System.out.print("Valor por hora: ");
                 BigDecimal valorPorHora = scanner.nextBigDecimal();
                 System.out.print("Duração (Horas): ");
                 Integer duracaoContrato = scanner.nextInt();
                 horaContratos.add(new HoraContrato(dataContrato, valorPorHora, duracaoContrato));
             }
+
+            // Cria o trabalhador com base nos dados digitados.
             Trabalhador trabalhador = new Trabalhador(nomeTrabalhador, senioridade, salarioBase, horaContratos, departamento);
             System.out.println();
-            System.out.print("Digite os dados para calcular a renda (MM/YYYY");
+
+
+            System.out.print("Digite os dados para calcular a renda (MM/YYYY): ");
             String mesRenda = scanner.nextLine();
         } catch (Exception e) {
             log.error(Utils.format("Aconteceu um erro ao executar o programa: {}", e.getMessage()), e);
