c3960ca2d8f75148ba729a6329f5392864adce94
Lucas Brenner <lucasbref82@gmail.com>
2025-10-16 21:34:57 -0300

    Arrumando a classe cliente

--- src/main/java/composicao/entidades/Cliente.java
+++ src/main/java/composicao/entidades/Cliente.java
@@ -5,8 +5,12 @@ import java.time.LocalDate;
 public class Cliente extends Pessoa {
     private String email;
 
-    public Cliente(String nome, LocalDate dataAniversario, String email) {
+    public Cliente(String nome, String email, LocalDate dataAniversario) {
         super(nome, dataAniversario);
         this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
