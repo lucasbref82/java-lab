5bab7f740e14537e45914d5bb161a5a82d4cef60
Lucas Brenner <lucasbref82@gmail.com>
2025-10-16 21:35:23 -0300

    Arrumando a classe pedido

--- src/main/java/composicao/entidades/Pedido.java
+++ src/main/java/composicao/entidades/Pedido.java
@@ -2,13 +2,18 @@ package composicao.entidades;
 
 import enums.StatusPedido;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
 public class Pedido {
-    private LocalDate momento;
+    private LocalDateTime momento;
     private StatusPedido statusPedido;
     private List<ItemPedido> itemPedidoList = new ArrayList<>();
     private Cliente cliente;
@@ -16,17 +21,17 @@ public class Pedido {
     public Pedido() {
     }
 
-    public Pedido(LocalDate momento, StatusPedido statusPedido, Cliente cliente) {
+    public Pedido(LocalDateTime momento, StatusPedido statusPedido, Cliente cliente) {
         this.momento = momento;
         this.statusPedido = statusPedido;
         this.cliente = cliente;
     }
 
-    public LocalDate getMomento() {
+    public LocalDateTime getMomento() {
         return momento;
     }
 
-    public void setMomento(LocalDate momento) {
+    public void setMomento(LocalDateTime momento) {
         this.momento = momento;
     }
 
@@ -50,8 +55,15 @@ public class Pedido {
         itemPedidoList.add(pedido);
     }
 
-    public void removePedido(ItemPedido pedido){
+    public void removeItemPedido(ItemPedido pedido){
         itemPedidoList.remove(pedido);
+    }
+
+    public BigDecimal total() {
+        return itemPedidoList.
+                stream()
+                .map(ItemPedido::subTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
     @Override
@@ -64,5 +76,22 @@ public class Pedido {
     @Override
     public int hashCode() {
         return Objects.hash(momento, statusPedido, itemPedidoList, cliente);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Momento do pedido: ").append(momento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))).append("\n");
+        stringBuilder.append("Status do pedido: ").append(statusPedido).append("\n");
+        stringBuilder.append("Cliente: ").append(cliente.getNome()).append(" ").append(cliente.getDataAniversario().format(DateTimeFormatter.ofPattern("dd/MM/yyy"))).append(" - ").append(cliente.getEmail()).append("\n");
+        stringBuilder.append("Itens do pedido: \n");
+        int itemPedidoSize = itemPedidoList.size();
+        int count = 0;
+        for (ItemPedido i : itemPedidoList) {
+            stringBuilder.append(i.getProduto().getNome()).append(", Quantidade: ").append(i.getQuantidade()).append(", Subtotal: ").append("$").append(i.subTotal().setScale(2, RoundingMode.HALF_UP)).append(itemPedidoSize != count ? "\n" : null);
+            ++count;
+        }
+        stringBuilder.append(total());
+        return stringBuilder.toString();
     }
 }
