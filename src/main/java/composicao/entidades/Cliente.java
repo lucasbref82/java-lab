dd5c42ec4d751cac3efcafc39cedc4368a353bcd
Lucas Brenner <lucasbref82@gmail.com>
2025-10-18 10:04:33 -0300

    Refatoração de classes

--- src/main/java/composicao/entidades/Cliente.java
+++ src/main/java/composicao/entidades/Cliente.java
@@ -1,8 +1,15 @@
 package composicao.entidades;
 
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
 public class Cliente extends Pessoa {
+
     private String email;
 
     public Cliente(String nome, String email, LocalDate dataAniversario) {
@@ -10,7 +17,16 @@ public class Cliente extends Pessoa {
         this.email = email;
     }
 
-    public String getEmail() {
-        return email;
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        Cliente cliente = (Cliente) object;
+        return Objects.equals(email, cliente.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), email);
     }
 }
