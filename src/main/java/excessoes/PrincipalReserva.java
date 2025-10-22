6ab7c59a1660c073e69ea284e1fccde3de161b65
Lucas Brenner <lucasbref82@gmail.com>
2025-10-22 06:18:10 -0300

    Criação de uma classe utilitaria para ajudar tratar somente datas

--- src/main/java/excessoes/PrincipalReserva.java
+++ src/main/java/excessoes/PrincipalReserva.java
@@ -10,6 +10,7 @@ public class PrincipalReserva {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             scanner.useLocale(Locale.US);
+
             System.out.print("Número do quarto: ");
             Integer numeroQuarto = scanner.nextInt();
             scanner.nextLine();
@@ -17,15 +18,14 @@ public class PrincipalReserva {
             String dataEntrada = scanner.nextLine();
             System.out.print("Data de saída (DD/MM/YYYY): ");
             String dataSaida = scanner.nextLine();
-
             ReservaHotel reservaHotel =
                     new ReservaHotel(
                             numeroQuarto,
                             Utils.parseLocalDate(dataEntrada),
                             Utils.parseLocalDate(dataSaida)
                     );
-
             System.out.println(reservaHotel);
+
             System.out.println();
 
             System.out.println("Digite a data para atualizar a reserva: ");
@@ -35,10 +35,8 @@ public class PrincipalReserva {
             dataSaida = scanner.nextLine();
             reservaHotel.atualizarDatas(Utils.parseLocalDate(dataEntrada), Utils.parseLocalDate(dataSaida));
             System.out.println(reservaHotel);
-
         } catch (Exception e) {
             System.out.println("Erro na reserva: " + e.getMessage());
-
         }
     }
 }
---
+++ src/main/java/utils/TimeUtils.java
@@ -0,0 +1,45 @@
+package utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+
+public class TimeUtils {
+
+    private TimeUtils() throws IllegalAccessException {
+        throw new IllegalAccessException("Classe utilitária " + getClass().getName() + " não pode ser instânciada.");
+
+    }
+
+    public static long diferencaDatasEmDias(LocalDate l1, LocalDate l2) {
+        validaDatasTemporais(l1, l2);
+        return ChronoUnit.DAYS.between(l1, l2);
+    }
+
+    public static long diferencaDataEmHoras(LocalDateTime l1, LocalDateTime l2) {
+        validaDatasTemporais(l1, l2);
+        return ChronoUnit.HOURS.between(l1, l2);
+    }
+
+    public static long diferencaEntreTemporais(Temporal t1, Temporal t2, ChronoUnit unit) {
+        if (t1 == null || t2 == null) throw new IllegalArgumentException("Temporal não pode ser nulo.");
+        return unit.between(t1, t2);
+    }
+
+    public static void validaDatasTemporais(Temporal d1, Temporal d2) {
+        if (d1 instanceof LocalDate && d2 instanceof LocalDate) {
+            if (((LocalDate) d1).isAfter((LocalDate) d2))
+                throw new IllegalArgumentException("Data inicial não pode ser posterior à data final.");
+            return;
+        }
+        if (d1 instanceof LocalDateTime && d2 instanceof LocalDateTime) {
+            if (((LocalDateTime) d1).isAfter((LocalDateTime) d2))
+                throw new IllegalArgumentException("Data inicial não pode ser posterior à data final.");
+            return;
+        }
+        throw new IllegalArgumentException("Tipos temporais diferentes ou não suportados (ex: LocalDate vs LocalDateTime).");
+    }
+
+}
+
