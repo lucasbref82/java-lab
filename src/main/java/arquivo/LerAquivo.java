77fa234dccadf90d3359f8e144d0bb01aece1dbd
Lucas Brenner <lucasbref82@gmail.com>
2025-11-03 07:37:19 -0300

    Leitura de arquivos

---
+++ src/main/java/arquivo/LerAquivo.java
@@ -0,0 +1,19 @@
+package arquivo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LerAquivo {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\temp\\entrada.txt");
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (IOException e1) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e1.getMessage());
+        }
+    }
+}
