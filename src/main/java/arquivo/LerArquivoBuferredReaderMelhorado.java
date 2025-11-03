48eac1dbb4580de6dc7dfabc8131603439db5712
Lucas Brenner <lucasbref82@gmail.com>
2025-11-03 07:51:13 -0300

    Melhorando a leitura de arquivos

---
+++ src/main/java/arquivo/LerArquivoBuferredReaderMelhorado.java
@@ -0,0 +1,19 @@
+package arquivo;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LerArquivoBuferredReaderMelhorado {
+    public static void main(String[] args) {
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("c:\\temp\\entrada.txt"))) {
+            String linha = bufferedReader.readLine();
+            while (linha != null) {
+                System.out.println(linha);
+                linha = bufferedReader.readLine();
+            }
+        } catch (IOException e1) {
+            System.out.println("Erro ao executar o programa: ");
+        }
+    }
+}
