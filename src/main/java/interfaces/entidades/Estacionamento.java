1b3d7501c810cf0a5589b22c9b6d56c2fc7aef84
Brenner <lucasbref82@gmail.com>
2025-11-06 00:35:20 -0300

    Ajuste da entidade

--- src/main/java/interfaces/entidades/Estacionamento.java
+++ src/main/java/interfaces/entidades/Estacionamento.java
@@ -1,21 +1,66 @@
 package interfaces.entidades;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
 public class Estacionamento {
+
     private String modeloCarro;
     private LocalDateTime entrada;
     private LocalDateTime saida;
+    private BigDecimal precoPorHora;
+    private BigDecimal precoPorDia;
 
+    public Estacionamento() {}
 
-    public Estacionamento() {
-    }
-
-    public Estacionamento(String modeloCarro, LocalDateTime entrada, LocalDateTime saida) {
+    public Estacionamento(String modeloCarro, LocalDateTime entrada, LocalDateTime saida, BigDecimal precoPorHora, BigDecimal precoPorDia) {
         this.modeloCarro = modeloCarro;
         this.entrada = entrada;
         this.saida = saida;
+        this.precoPorHora = precoPorHora;
+        this.precoPorDia = precoPorDia;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String modeloCarro;
+        private LocalDateTime entrada;
+        private LocalDateTime saida;
+        private BigDecimal precoPorHora;
+        private BigDecimal precoPorDia;
+
+        public Builder modeloCarro(String modeloCarro) {
+            this.modeloCarro = modeloCarro;
+            return this;
+        }
+
+        public Builder entrada(LocalDateTime entrada) {
+            this.entrada = entrada;
+            return this;
+        }
+
+        public Builder saida(LocalDateTime saida) {
+            this.saida = saida;
+            return this;
+        }
+
+        public Builder precoPorHora(BigDecimal precoPorHora) {
+            this.precoPorHora = precoPorHora;
+            return this;
+        }
+
+        public Builder precoPorDia(BigDecimal precoPorDia) {
+            this.precoPorDia = precoPorDia;
+            return this;
+        }
+
+        public Estacionamento build() {
+            return new Estacionamento(modeloCarro, entrada, saida, precoPorHora, precoPorDia);
+        }
     }
 
     public String getModeloCarro() {
@@ -42,15 +87,32 @@ public class Estacionamento {
         this.saida = saida;
     }
 
+    public BigDecimal getPrecoPorHora() {
+        return precoPorHora;
+    }
+
+    public void setPrecoPorHora(BigDecimal precoPorHora) {
+        this.precoPorHora = precoPorHora;
+    }
+
+    public BigDecimal getPrecoPorDia() {
+        return precoPorDia;
+    }
+
+    public void setPrecoPorDia(BigDecimal precoPorDia) {
+        this.precoPorDia = precoPorDia;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Estacionamento that = (Estacionamento) o;
-        return Objects.equals(modeloCarro, that.modeloCarro) && Objects.equals(entrada, that.entrada) && Objects.equals(saida, that.saida);
+        return Objects.equals(modeloCarro, that.modeloCarro) && Objects.equals(entrada, that.entrada) && Objects.equals(saida, that.saida) && Objects.equals(precoPorHora, that.precoPorHora) && Objects.equals(precoPorDia, that.precoPorDia);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(modeloCarro, entrada, saida);
+        return Objects.hash(modeloCarro, entrada, saida, precoPorHora, precoPorDia);
     }
 }
