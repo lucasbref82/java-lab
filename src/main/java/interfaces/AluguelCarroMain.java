725d90667b71fa49f216f6d36226a01343f51067
Brenner <lucasbref82@gmail.com>
2025-11-10 09:44:20 -0300

    Removendo servico e dependências desnecessárias

--- src/main/java/interfaces/AluguelCarroMain.java
+++ src/main/java/interfaces/AluguelCarroMain.java
@@ -1,18 +1,15 @@
 package interfaces;
 
 import interfaces.model.entidades.AluguelCarro;
-import interfaces.model.entidades.Fatura;
 import interfaces.model.entidades.Veiculo;
-import interfaces.service.AluguelCarroService;
 import utils.TimeUtils;
 
-import java.math.BigDecimal;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AluguelCarroMain {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)){
+        try (Scanner scanner = new Scanner(System.in)) {
 
             scanner.useLocale(Locale.US);
 
@@ -25,12 +22,6 @@ public class AluguelCarroMain {
             System.out.print("Data saída (dd/MM/yyyy hh:mm): ");
             String dataSaida = scanner.nextLine();
 
-            System.out.print("Digite o preço do pagamento básico:  ");
-            BigDecimal pagamentoBasico = scanner.nextBigDecimal();
-
-            System.out.println("Digite a taxa: ");
-            BigDecimal taxa = scanner.nextBigDecimal();
-
             AluguelCarro aluguelCarro =
                     AluguelCarro.builder()
                             .entrada(TimeUtils.parserLocalDateTime(dataEntrada, TimeUtils.LOCAL_DATE_TIME_PT_BR))
@@ -38,11 +29,8 @@ public class AluguelCarroMain {
                             .veiculo(Veiculo.builder()
                                     .nome(modeloCarro)
                                     .build()
-                            ).fatura(Fatura.builder()
-                                    .pagamentoBasico(pagamentoBasico)
-                                    .taxa(taxa)
-                                    .build()
                             ).build();
+
         } catch (Exception e) {
             System.out.println("Ocorreu um errro na aplicação: ");
         }
