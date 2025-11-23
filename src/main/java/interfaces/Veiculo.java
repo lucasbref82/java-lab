e81aeb12976d8e74ad8fad20fa26544a5bb952a2
Brenner <lucasbref82@gmail.com>
2025-11-23 01:53:17 -0300

    Entidade veiculo

---
+++ src/main/java/interfaces/Veiculo.java
@@ -0,0 +1,35 @@
+package interfaces;
+
+import java.util.Objects;
+
+public class Veiculo {
+
+    private String modelo;
+
+    public Veiculo() {
+    }
+
+    public Veiculo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Veiculo veiculo = (Veiculo) o;
+        return Objects.equals(modelo, veiculo.modelo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(modelo);
+    }
+}
